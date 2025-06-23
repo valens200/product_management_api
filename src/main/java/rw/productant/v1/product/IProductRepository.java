a99a36baa8cc5737013b655ef43a4887dbedc29b
valens200 <valensniyonsenga2003@gmail.com>
2025-06-23 11:14:54 +0200

    refact: optimized the global code for better readability

--- src/main/java/rw/productant/v1/product/IProductRepository.java
+++ src/main/java/rw/productant/v1/product/IProductRepository.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 @Repository
 public interface IProductRepository extends JpaRepository<Product, UUID> {
     Product findByName(String name);
+    boolean existsByName(String name);
 }
--- src/main/java/rw/productant/v1/product/ProductController.java
+++ src/main/java/rw/productant/v1/product/ProductController.java
@@ -11,37 +11,37 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/products")
 @Slf4j
 public class ProductController {
 
     @Autowired
     private ProductServiceImplementation productService;
     @PostMapping
-    public ResponseEntity<ApiResponsePayload> create(@RequestBody @Valid  ProductDto productDto) {
+    public ResponseEntity<ApiResponsePayload> createProduct(@RequestBody @Valid  ProductDto productDto) {
         Product createdProduct = this.productService.createProduct(productDto);
         return ResponseEntity.ok().body(new ApiResponsePayload(true,"The product was created successfully",createdProduct));
     }
     @GetMapping
-    public ResponseEntity<ApiResponsePayload> getAll() {
+    public ResponseEntity<ApiResponsePayload> getAllProducts() {
         List<Product> productsResponse = this.productService.getAllProducts();
         return ResponseEntity.ok().body(new ApiResponsePayload(true, "Products fetched successfully", productsResponse));
     }
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponsePayload> getById(@PathVariable UUID id) {
+    public ResponseEntity<ApiResponsePayload> getProductById(@PathVariable UUID id) {
         Product productResponse = this.productService.getProductById(id);
         return ResponseEntity.ok().body(
                 new ApiResponsePayload(true,"Successfully fetched a product", productResponse)
         );
     }
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponsePayload> update(@PathVariable UUID id, @RequestBody @Valid ProductDto productDto) {
+    public ResponseEntity<ApiResponsePayload> updateProduct(@PathVariable UUID id, @RequestBody @Valid ProductDto productDto) {
         Product updatedProduct = this.productService.updateProductById(id,productDto);
         return ResponseEntity.ok().body(new ApiResponsePayload(true,"The product was updated successfully", updatedProduct));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponsePayload> delete(@PathVariable UUID id) {
+    public ResponseEntity<ApiResponsePayload> deleteProduct(@PathVariable UUID id) {
         productService.deleteProductById(id);
         return ResponseEntity.ok().body(new ApiResponsePayload(true, "The product was successfully deleted"));
     }
